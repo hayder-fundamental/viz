b848fd934952a32513a9b82ba3dad16f99e1f821
Hayder <hayder@fundamental.tech>
2025-09-18 14:44:53 +0200

    updates to metric name indirection

--- constants.py
+++ constants.py
@@ -1,3 +1,9 @@
+import enum
+
+# Can use these to reproduce the offline eval score weightings.
+offline_eval_num_context_probs = [1.0, 0.75818182, 0.24, 0.13636364]
+
+
 class Tags:
     classification = "classification"
     regression = "regression"
@@ -7,27 +13,93 @@ class Tags:
     ms4_sota = "Hayder::MS4-SOTA"
 
 
+class RunStatus(enum.StrEnum):
+    RUNNING = "running"
+    FINISHED = "finished"
+    # , , crashed, killed, preempting, preempted
+
+
 class MetricNames:
-    _eip_stub = "evaluation_Improvement probability over TabPFNv2fixed/score at  -"
-    # works
-    t_cross_entropy = "training/CrossEntropyLoss"
+    class OnlineEval:
+        _eip_stub = "evaluation_Improvement probability over TabPFNv2/score"
+        _acc = "at  - accuracy"
+        _mse = "at  - mse"
 
-    eip_acc = (
-        "evaluation_Improvement probability over TabPFNv2fixed/score at  - accuracy"
-    )
-    eip_acc_32 = f"{_eip_stub} accuracy with 32 num_context"
-    eip_acc_128 = f"{_eip_stub} accuracy with 128 num_context"
-    eip_acc_1024 = f"{_eip_stub} accuracy with 1024 num_context"
-    eip_acc_7500 = f"{_eip_stub} accuracy with 7500 num_context"
+        eip_acc = f"{_eip_stub} {_acc}"
+        eip_acc_32 = f"{_eip_stub} {_acc} with 32 num_context"
+        eip_acc_128 = f"{_eip_stub} {_acc} with 128 num_context"
+        eip_acc_1024 = f"{_eip_stub} {_acc} with 1024 num_context"
+        eip_acc_7500 = f"{_eip_stub} {_acc} with 7500 num_context"
+        eip_acc_10000 = f"{_eip_stub} {_acc} with 10000 num_context"
+        eip_mse = f"{_eip_stub} {_mse}"
+        eip_mse_32 = f"{_eip_stub} {_mse} with 32 num_context"
+        eip_mse_128 = f"{_eip_stub} {_mse} with 128 num_context"
+        eip_mse_1024 = f"{_eip_stub} {_mse} with 1024 num_context"
+        eip_mse_7500 = f"{_eip_stub} {_mse} with 7500 num_context"
+        eip_score = "evaluation_Improvement probability over TabPFNv2/main score"
+        eip_score_128 = f"{_eip_stub} with 128 num_context"
+        eip_score_1024 = f"{_eip_stub} with 1024 num_context"
+        eip_score_7500 = f"{_eip_stub} with 7500 num_context"
+        eip_score_10000 = f"{_eip_stub} with 10000 num_context"
 
-    eip_accs = [
-        eip_acc,
-        eip_acc_32,
-        eip_acc_128,
-        eip_acc_1024,
-        eip_acc_7500,
-    ]
+        eip_acc_by_ctx = [
+            eip_acc_128,
+            eip_acc_1024,
+            eip_acc_7500,
+            eip_acc_10000,
+        ]
 
-    eip_mse = "evaluation_Improvement probability over TabPFNv2fixed/score at  - mse"
-    t_huber = "training/HuberLoss"
-    e_huber = "evaluation/HuberLoss"
+        eip_mse_by_ctx = [
+            eip_mse_128,
+            eip_mse_1024,
+            eip_mse_7500,
+        ]
+
+        eip_score_by_ctx = [
+            eip_score_128,
+            eip_score_1024,
+            eip_score_7500,
+            eip_score_10000,
+        ]
+
+        t_cross_entropy = "training/CrossEntropyLoss"
+        t_huber = "training/HuberLoss"
+        e_huber = "evaluation/HuberLoss"
+
+    class OfflineEval:
+        eip_score = "Improvement probability over TabPFNv2 (tabpfn-v2)/main score"  # noqa: E501
+        eip_score_128 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score with 128 num_context"  # noqa: E501
+        eip_score_1024 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score with 1024 num_context"  # noqa: E501
+        eip_score_7500 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score with 7500 num_context"  # noqa: E501
+        eip_score_10000 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score with 10000 num_context"  # noqa: E501
+        eip_acc = (  # noqa: E501
+            "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - accuracy"  # noqa: E501
+        )  # noqa: E501
+        eip_acc_128 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - accuracy with 128 num_context"  # noqa: E501
+        eip_acc_1024 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - accuracy with 1024 num_context"  # noqa: E501
+        eip_acc_7500 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - accuracy with 7500 num_context"  # noqa: E501
+        eip_acc_10000 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - accuracy with 10000 num_context"  # noqa: E501
+        eip_mse = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - mse"  # noqa: E501
+        eip_mse_128 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - mse with 128 num_context"  # noqa: E501
+        eip_mse_1024 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - mse with 1024 num_context"  # noqa: E501
+        eip_mse_7500 = "Improvement probability over TabPFNv2 (tabpfn-v2)/score at  - mse with 7500 num_context"  # noqa: E501
+
+        eip_acc_by_ctx = [
+            eip_acc_128,
+            eip_acc_1024,
+            eip_acc_7500,
+            eip_acc_10000,
+        ]
+
+        eip_mse_by_ctx = [
+            eip_mse_128,
+            eip_mse_1024,
+            eip_mse_7500,
+        ]
+
+        eip_score_by_ctx = [
+            eip_score_128,
+            eip_score_1024,
+            eip_score_7500,
+            eip_score_10000,
+        ]
