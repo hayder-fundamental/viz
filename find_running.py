9ceb3a8ac891c4a4a7385accc864c57b9cac9ff5
Hayder <hayder@fundamental.tech>
2025-09-19 15:25:53 +0200

    update script to show whiche evals runing when training run not running

--- find_running.py
+++ find_running.py
@@ -28,6 +28,10 @@ def cmd_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def train_run_id_from_eval_id(s: str) -> str:
+    return s.split("_")[0]
+
+
 if __name__ == "__main__":
     args = cmd_args()
     logger = logging.getLogger()
@@ -57,10 +61,10 @@ if __name__ == "__main__":
 
     eval_names = [run.name for run in eval_running]
     train_running_ids = {run.id for run in train_running}
-    trains_with_eval = {s.split("_")[0] for s in eval_names}
+    trains_with_eval = {train_run_id_from_eval_id(s) for s in eval_names}
 
     def print_row(run):
-        print(f"{run.name:<75} {run.id:>10}")
+        print(f"{run.name:<110} {run.id:>10}")
 
     print("Train running:")
     for run in train_running:
@@ -72,6 +76,11 @@ if __name__ == "__main__":
     print()
     print("Training runs without running evals:")
     for run in train_running:
-        if run.id in trains_with_eval:
-            continue
-        print_row(run)
+        if run.id not in trains_with_eval:
+            print_row(run)
+    print()
+    print("Eval runs on training runs which aren't running:")
+    for run in eval_running:
+        train_run_id = train_run_id_from_eval_id(run.name)
+        if train_run_id not in train_running_ids:
+            print_row(run)
